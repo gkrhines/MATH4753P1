1e7f6ce8676f976827108047eae99bbf5194d82c
gkrhines <gkrhines21@ou.edu>
2020-09-15 11:59:15 -0500

    Initial file commit

---
+++ R/myF.R
@@ -0,0 +1,14 @@
+#' Square Function
+#'
+#'Takes a vector and returns a vector of squared components.
+#'
+#' @param x
+#'
+#' @return a vector of squared components
+#' @export
+#'
+#' @examples
+myF <- function(x)
+{
+  x^2
+}
